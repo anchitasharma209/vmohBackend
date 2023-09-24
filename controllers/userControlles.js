5d28062dc94ae18318ee70da62162df8d28ef1c2
anchitasharma209 <anchitasharma209@gmail.com>
2023-09-24 14:26:53 +0530

    API moficitaions for email during signup + reset via phone number

--- controllers/userControlles.js
+++ controllers/userControlles.js
@@ -1,26 +1,30 @@
 const Users = require("../models/userModel");
 const jwt = require("jsonwebtoken");
-const secretKey = "eoubouYBiycenauocbaonpicne";
+const { validationResult } = require("express-validator");
+const mongoose = require("mongoose");
+const moment = require("moment");
+const secretKey =
+  "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5NTQ1OTAyMiwiaWF0IjoxNjk1NDU5MDIyfQ.AV9LAkze8oxNJR9yv4oHb2geqvne4a6aKoHTXXxpl1g";
 const { generateOTP } = require("../utils/otp");
 const { sendSMS } = require("../utils/sms");
-const { validationResult } = require("express-validator");
 
 // SIGN UP API creating new users
 exports.signUp = async (req, res) => {
   const {
     firstName,
     lastName,
+    email,
     companyName,
     phoneNumber,
     password,
     confirmPassword,
   } = req.body;
-
   // Validate request
   if (
     !firstName ||
     !lastName ||
     !companyName ||
+    !email ||
     !phoneNumber ||
     !password ||
     !confirmPassword
@@ -38,32 +42,42 @@ exports.signUp = async (req, res) => {
     if (user) {
       return res.status(400).send({
         status: false,
-        message: "Phone number already exists in the database",
+        message: "Phone number already exists",
+      });
+    }
+    let userEmail = await Users.findOne({ email });
+    if (userEmail) {
+      return res.status(400).send({
+        status: false,
+        message: "Email already exists!",
       });
     }
 
     // Generate OTP
     const otp = generateOTP();
+    const otpExpiration = moment().add(5, "minutes").toDate(); // 5 minutes timer
 
     // Save user details and OTP to the database
     user = new Users({
       firstName,
       lastName,
+      email,
       companyName,
       phoneNumber,
       password,
       otp,
+      otpExpiration,
       status: false,
     });
     await user.save();
 
     // Send OTP to the user's phone number
-    const message = `Your OTP for sign-up is ${otp}`;
+    const message = `Your OTP for sign-up is ${otp} and this is valid for 5 minutes`;
     sendSMS(phoneNumber, message);
-
+    console.log(message);
     return res.status(200).send({
       status: true,
-      message: "OTP has been sent. Please verify your Number !",
+      message: "OTP has been sent. Please verify your Number!",
       data: user,
     });
   } catch (err) {
@@ -77,7 +91,6 @@ exports.signUp = async (req, res) => {
 // API To verrify OTP
 exports.verifyOtp = async (req, res) => {
   const { phoneNumber, otp } = req.body;
-
   try {
     // Find the user by phone number
     const user = await Users.findOne({ phoneNumber: phoneNumber });
@@ -88,7 +101,7 @@ exports.verifyOtp = async (req, res) => {
       });
     }
 
-    // Check if OTP matches
+    // Check if otp matches
     if (user.otp !== otp) {
       return res.status(400).json({
         status: false,
@@ -96,6 +109,16 @@ exports.verifyOtp = async (req, res) => {
       });
     }
 
+    // Check if the otp time has expired
+    const otpTimestamp = moment(user.otpExpiration);
+    const currentTime = moment();
+    const otpValidityDuration = moment.duration(currentTime.diff(otpTimestamp));
+    if (otpValidityDuration.asMinutes() > 5) {
+      return res.status(400).json({
+        status: false,
+        message: "OTP has expired",
+      });
+    }
     // OTP verified successfully, update the user document
     user.otp = "";
     user.status = true;
@@ -161,33 +184,38 @@ exports.login = (req, res) => {
         status: false,
         message: err.message,
       });
-    }); 
+    });
 };
 
-// reseting password
+// reseting password by phoneNumber
 exports.resetPassword = async (req, res) => {
-  const id = req.params.id;
-  const { password, confirmPassword } = req.body;
+  const phoneNumber = req.body.phoneNumber;
 
-  const user = await Users.findById(id);
-  console.log(user.password, "user.password");
-
-  if (password != confirmPassword) {
-    return res.status(400).send({
+  const user = await Users.findOne({ phoneNumber: phoneNumber });
+  if (!user) {
+    return res.status(404).json({
       status: false,
-      message: "Password and confirm password do not match",
+      message: "User not found",
     });
   }
 
-  if (password === user.password) {
-    return res.status(400).send({
-      status: false,
-      message: "New Password is same as prevoius !",
-    });
-  }
+  const otp = generateOTP();
 
-  Users.findByIdAndUpdate(
-    id,
+  // Send OTP to the user's phone number
+  const message = `Your OTP for Reset Password is ${otp} and this is valid for 5 minutes`;
+  sendSMS(phoneNumber, message);
+  return res.status(200).send({
+    status: true,
+    message: "OTP has been sent for reset password",
+    phoneNumber,
+  });
+};
+
+// Update Password after matching OTP that is sent at the time of passwrod resetting
+exports.updatePassword = (req, res) => {
+  const phoneNumber = req.body.phoneNumber;
+  Users.updateOne(
+    { phoneNumber: phoneNumber },
     {
       $set: {
         password: password,
@@ -200,7 +228,7 @@ exports.resetPassword = async (req, res) => {
       if (!user) {
         return res.status(404).send({
           status: false,
-          message: `User not found with id ${id}`,
+          message: `User not found with id ${phoneNumber}`,
         });
       }
       return res.status(200).send({
@@ -217,25 +245,36 @@ exports.resetPassword = async (req, res) => {
     });
 };
 
-/*  UUdating user details
+/*  Updating user details
  */
 exports.updateUser = async (req, res) => {
   try {
     const userId = req.decoded.id;
-    const { firstName, lastName, companyName, phoneNumber } = req.body;
-    const updatedUser = await Users.findByIdAndUpdate(
-      userId,
-      { firstName, lastName, companyName, phoneNumber },
-      { new: true }
+
+    const { firstName, lastName, companyName } = req.body;
+    // Create an object with the fields to update
+    const updateFields = {};
+    if (firstName) {
+      updateFields.firstName = firstName;
+    }
+    if (lastName) {
+      updateFields.lastName = lastName;
+    }
+    if (companyName) {
+      updateFields.companyName = companyName;
+    }
+    // Use the _id filter to update the document
+    const updatedUser = await Users.updateOne(
+      { _id: new mongoose.Types.ObjectId(userId) },
+      { $set: updateFields }
     );
 
-    if (!updatedUser) {
+    if (updatedUser.nModified === 0) {
       return res.status(404).json({
         status: false,
         message: "User not found",
       });
     }
-
     res.status(200).json({
       status: true,
       message: "UserDetails  updated successfully",
@@ -258,20 +297,7 @@ exports.generateOtp = async (req, res) => {
 
   const { phoneNumber } = req.body;
   try {
-    // const user = await Users.findOne({ phoneNumber });
-    // if (!user) {
-    //   return res.status(404).json({
-    //     status: false,
-    //     message: "User not found",
-    //   });
-    // }
-
-    // Generate OTP and save it to the user's document in the database
     const otp = generateOTP();
-    // user.otp = otp;
-    // await user.save();
-
-    // Send OTP to user's phone number
     const message = `Your OTP for resetting the password is ${otp}`;
     sendSMS(phoneNumber, message);
 
