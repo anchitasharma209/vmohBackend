03d75807b7be5067318dfc6a81f78e9d31b96784
anchitasharma209 <anchitasharma209@gmail.com>
2023-09-24 14:25:48 +0530

    added readme

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-const express = require('express');
-const mongoose= require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 // DATABASE CONNECTIONS
-mongoose.connect('mongodb+srv://anchitasharma209:Qy6BFw1fYUWgqEI7@cluster0.kogmfqh.mongodb.net/?retryWrites=true&w=majority',{
-    useNewURLParser: true,
-    useUnifiedTopology: true,
-}).then((data)=>{
+mongoose
+  .connect(
+    "mongodb+srv://anchitasharma209:Qy6BFw1fYUWgqEI7@cluster0.kogmfqh.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewURLParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((data) => {
     console.log(`mongodb connection established ${data.connection.host}`);
-}).catch((error)=>{
+  })
+  .catch((error) => {
     console.log(`connection error: ${error}`);
-})
-
+  });
 
 app.use(express.json());
-const port = process.env.PORT ||8081;
+const port = process.env.PORT || 8081;
 
-const user = require('./routes/userRoutes');
-const listing = require('./routes/propertyRoutes');
-app.use ("/api/v1", user,listing);
+const user = require("./routes/userRoutes");
+const listing = require("./routes/propertyRoutes");
+app.use("/api/v1", user, listing);
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
-  });
+  console.log(`Server is running on port ${port}.`);
+});
